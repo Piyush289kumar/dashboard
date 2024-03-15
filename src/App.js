4aee6a5859e5090336d99a704372e7e256216525
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-15 16:03:02 +0530

    Working

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 function App() {
+  const activeMenu = false;
   return (
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
-              <button>
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray rounded-full text-white"
+                style={{ background: "blue" }}
+              >
                 <FiSettings />
               </button>
             </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Siderbar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Siderbar w-0</div>
+          )}
+          <div
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              NavBar
+            </div>
           </div>
         </div>
       </BrowserRouter>
