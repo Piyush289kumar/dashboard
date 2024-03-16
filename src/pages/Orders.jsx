ec6a41a1b6d679fd95ed8762486d865265cecaad
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-16 17:38:51 +0530

    Orders Build

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 function Orders() {
+  let grid;
+  const toolbar = ["PdfExport"];
+  const toolbarClick = (args) => {
+    grid.pdfExport("CurrentPage");
+  };
   return (
-    <div>Orders</div>
-  )
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowFiltering
+        allowSorting
+        allowExcelExport
+        toolbar={toolbar}
+        allowPdfExport={true}
+        toolbarClick={toolbarClick}
+        ref={(g) => (grid = g)}
+        allowResizing
+        contextMenuItems={contextMenuItems}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, idx) => (
+            <ColumnDirective key={idx} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            PdfExport,
+            Toolbar,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
 }
 
-export default Orders
+export default Orders;
