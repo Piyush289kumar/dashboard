88cd1aa17e33008eac9d8a7aff76425120915dee
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-16 12:05:53 +0530

    Working on Ecommer Page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useStateContext } from "../contexts/ContextProvider";
 
-function Button() {
+function Button({
+  icon,
+  color,
+  bgColor,
+  bgHoverColor,
+  size,
+  text,
+  borderRadius,
+  width,
+}) {
+  const { setIsClicked, initializationState } = useStateContext();
   return (
-    <div>Button</div>
-  )
+    <button
+      type="button"
+      onClick={() => setIsClicked(initializationState)}
+      style={{
+        backgroundColor: bgColor,
+        color: color,
+        borderRadius: borderRadius,
+      }}
+      className={`text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
+    >
+      {icon} {text}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
