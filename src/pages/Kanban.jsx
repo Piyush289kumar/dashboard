c637c15bba8417d4ebb7a13c9e0bfb156490be29
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-16 21:18:58 +0530

    KanbanComponent Build

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
 
 function kanban() {
   return (
-    <div>kanban</div>
-  )
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category={"App"} title={"Task Progress"} />
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, idx) => (
+            <ColumnDirective key={idx} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
 }
 
-export default kanban
+export default kanban;
