95e6aa5e67c4b80775faba2c8ccef61c8955aa61
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-17 11:33:37 +0530

    working on Theme

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,14 +6,15 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 function Sidebar() {
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize, currentColorMode } =
+    useStateContext();
   const handleCloseSideBar = () => {
     if (activeMenu !== undefined && screenSize <= 900) {
       setActiveMenu(false);
     }
   };
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white text-md m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
   return (
@@ -47,6 +48,9 @@ function Sidebar() {
                 to={`/${link.name}`}
                 key={linkIdx}
                 onClick={handleCloseSideBar}
+                style={({ isActive }) => ({
+                  backgroundColor: isActive ? currentColorMode : "",
+                })}
                 className={({ isActive }) =>
                   isActive ? activeLink : normalLink
                 }
